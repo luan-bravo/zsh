43bbf726d2e778dc6573abb7f4af573ec8d75ea4
luan-brav0 <luan-brav0@github.com>
2025-03-05 21:58:18 -0300

    keybindings custom file

---
+++ custom/keybinds.zsh
@@ -0,0 +1,6 @@
+# Custom keybindings by luan-brav0
+
+bindkey '^H' backward-kill-word # <C-<backspace>> deletes words to the left
+bindkey '^W' backward-kill-word # <C-<delete>> deletes words to the right
+bindkey '^[[3;5~' kill-word # <C-<backspace>> deletes words to the left
+bindkey "^M" accept-line # fix enter (can't remember when or why added this, but gonna keep it here)
