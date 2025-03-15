cdc62cc16ed4df944eeb40510d16aa90b4e55efd
luan-brav0 <luan-brav0@github.com>
2025-03-15 15:13:17 -0300

    Now THIS fixes the todo function; Minor tweaks

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,18 +1,25 @@
-# aliases by luan-brav0
+# Aliases by luan-brav0
 
+# System aliases
 alias c="clear" # Clean your room
 alias q="exit"
-alias f="fzf"
+
+alias nv="nvim"
+alias v="nvim"
 
 alias stdn="shutdown now"
-alias il="i3lock"
 
+alias il="i3lock"
 alias py="python"
+# alias hx="helix" # not that great. nvim ftw
+
 alias ino="arduino-cli"
 
-# not that great. nvim ftw
-# alias hx="helix"
+del() {
+    mv "$*" "$HOME/.trash"
+}
 
+# Git
 alias gaa="git add -A"
 alias gap="git add -p"
 alias gs="git status"
@@ -29,19 +36,24 @@ gsync (){
     git push
 }
 
-# exa aliases - ls made with rust
+
+# exa aliases - ls made with rust and a much nicer
 alias x="exa -l -h -n -s='type' --icons"
 alias xa="x -a"
 alias xt="x -T"
 alias xta="xa -T"
 
+
 # Fix overscan (when using old HDMI TV as monitor)
 alias osfix="xrandr --output HDMI-A-0 --set underscan on & xrandr --output HDMI-A-0 --set 'underscan hborder' 80 --set 'underscan vborder' 40"
+
 
 # iwd wifi manager aliases
 alias iwpower="rfkill unblock all && iwctl device wlan0 set-property Powered on"
 alias iwshow="iwctl station wlan0 show"
 alias iwscan="iwctl station wlan0 get-networks"
+
+# Keyboard layouts
 alias huebr="setxkbmap br"
 alias merica="setxkbmap us"
 alias inter="setxkbmap -layout us -variant intl"
@@ -53,6 +65,7 @@ zshconfig() {
     popd > /dev/null
 }
 alias zconfig="zshconfig"
+alias zconf="zconfig"
 
 # alias nvimconfig="nvim $HOME/config/nvim"
 nvimconfig() {
@@ -62,16 +75,16 @@ nvimconfig() {
 }
 
 
-# note taking
+# Note taking
 mknote() {
     echo "# TITLE: $1\n\n# DATE: $(date +"%y/%m/%d")\n\n# TIME: $(date +"%H:%M")\n" >> ./"$(date +"%y%m%d%H%M")--$1".md
     nvim ./"$(date +"%y%m%d%H%M")--$1".md
 }
 
 
-# push todo strings to todoFile (it was a very good source of shell scripting learning for me)
+# Push todo strings to todoFile (it was a very good source of shell scripting learning for me)
 todo() {
-    # set default global todofile or custom with '-f' flag
+    # Set default global todofile or custom with '-f' flag
     local todoFile="$TODOFILE"
     while getopts "f:" opt; do
         case $opt in
@@ -80,16 +93,16 @@ todo() {
             ;;
             \?) echo "Invalid option: -$OPTARG" ;;
         esac
-        # takes the flag argument out of the arg array
+        # Takes the flag argument out of the arg array
         shift $((OPTIND - 1))
     done
 
-    # if todoFile doesn't exists, create it
+    # If todoFile doesn't exists, create it
     if [[ ! -f "$todoFile" ]]; then
         echo "${yellow}No file $todoFile currently exists. Touching todoFile.${nc}"
         touch "$todoFile"
     fi
-    # adds a new line if there are already other todos in file
+    # Adds a new line if there are already other todos in file
     if [[ -s "$todoFile" ]]; then
         echo "" >> "$todoFile"
     fi
@@ -103,17 +116,26 @@ todo() {
         fi
     done
 
-
     # Total incomplete todos
-    local todoCount=$(grep -v -i '^[[:space:]]*- \[[xX]\]' "$todoFile" | grep -v '^[[:space:]#]*$' | tee "$todoFile" | wc -l)
-    if $added; then
+    local todoCount=$(grep -c -v '^[[:space:]#]*$' "$todoFile")
+    # Echo staturs
+    if [[ $added -gt 0 ]]; then
         echo "todo: ${green}$added todos added out of $#! Total incomplete todos: $todoCount.${nc}"
+        return 0
+    elif [[ $added -st $# ]]; then
+        echo "todo: ${yellow}$added todos added out of $#! Total incomplete todos: $todoCount.${nc}"
+        return 1
     else
         echo "todo: ${red} No new todos added! Total incomplete todos: $todoCount.${nc}"
+        return 1
     fi
 }
 
 
+# TODO: Check if this still is necessary or if just the alias is enough
+# Exporting and sourcing added to $ZSH_CUSTOM/exports.zsh file
+alias idf="idf.py"
+: << 'IDF'
 idf() {
     if [ ! -x "$(command -v idf.py)" ]; then
         echo "no idf.py"
@@ -127,10 +149,12 @@ idf() {
         # TODO: wtf is this line doing inside and ouside of the if statement? what crack was I smoking?
         source "$IDF_PATH/export.sh"
     fi
-    idf.py $@
+    idf.py "$*"
 }
+IDF
 
 
 potp() {
-    pass otp $1
+    [[ $# -gt 1 ]] && { echo "potp: ${red}Too many arguments.${nc}"; return 1 }
+    pass otp "$1"
 }
