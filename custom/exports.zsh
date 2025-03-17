276a98a793d30283aea61a8873827a2ace23f890
luan-brav0 <luan-brav0@github.com>
2025-03-17 10:54:46 -0300

    xpt function modified and implemented (not sure if will be kept tho)

--- custom/exports.zsh
+++ custom/exports.zsh
@@ -1,41 +1,47 @@
 # Exports
 
 src() {
-    [[ -s "$1" ]] && source "$*"
+    [[ -f "$1" ]] && source "$1"
 }
 xpt() {
     [[ -d "$2" ]] && export "$1"="$2"
 }
 
-export IDF_PATH="/opt/esp-idf"
+
+xpt IDF_PATH "/opt/esp-idf"
 src "$IDF_PATH/export.sh"
 
 # Exports for set up
-export GIT_CONFIG_GLOBAL="$HOME/.config/.gitconfig"
-src "$ZDOTDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" 2>"/dev/null"
+xpt GIT_CONFIG_GLOBAL "$HOME/.config/.gitconfig"
+# src "$ZDOTDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # now in plugins
 
 # Path and sourcing
 if [ -d "$HOME/.local/share/adb-fastboot/platform-tools" ] ; then
-    export PATH="$PATH:$HOME/.local/share/adb-fastboot/platform-tools"
+    xpt PATH "$PATH:$HOME/.local/share/adb-fastboot/platform-tools"
 fi
 
 src "$HOME/.bun/_bun"
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$PATH:$BUN_INSTALL/bin"
+xpt BUN_INSTALL "$HOME/.bun"
+xpt PATH "$PATH:$BUN_INSTALL/bin"
 
-export ANDROID_HOME="$HOME/Android/sdk"
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
+xpt ANDROID_HOME "$HOME/Android/sdk"
+xpt PATH "$PATH:$ANDROID_HOME/emulator"
+xpt PATH "$PATH:$ANDROID_HOME/platform-tools"
 
-export FLYCTL_INSTALL="$HOME/.fly"
-export PATH="$PATH:$FLYCTL_INSTALL/bin"
+xpt FLYCTL_INSTALL "$HOME/.fly"
+xpt PATH "$PATH:$FLYCTL_INSTALL/bin"
 
-export PATH="$PATH:$HOME/.turso"
+xpt PATH "$PATH:$HOME/.turso"
 
-export PNPM_HOME="$HOME/.local/share/pnpm"
+xpt PNPM_HOME "$HOME/.local/share/pnpm"
 case ":$PATH:" in
     # find if this case is really necessary, if it isn't, remove the echo in the next line, else modify every $PATH update in this file to use a case statement
   *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PATH:$PNPM_HOME" ;;
+  *) xpt PATH "$PATH:$PNPM_HOME" ;;
 esac
-export PATH="$PATH:$ZDOTDIR/custom/bin"
+xpt PATH "$PATH:$ZDOTDIR/custom/bin"
+
+# WSL browser, requires wslu installation to be followerd
+if [[ -n $WSL_DISTRO_NAME ]]; then
+    xpt BROWSER "wslview"
+fi
