3050ffcce31c7c18f6a68dfab2cf542f9d218740
luan-bravo <luan-bravo@github.com>
2025-04-02 22:34:16 -0300

    Create nvr to export dirs to PATH & use it to fix bun sourcing

--- custom/exports.zsh
+++ custom/exports.zsh
@@ -1,11 +1,8 @@
 # Exports
 
-src() {
-    [[ -f "$1" ]] && source "$1"
-}
-xpt() {
-    [[ -d "$2" ]] && export "$1"="$2"
-}
+src() { [[ -f "$1" ]] && source "$1" }
+xpt() { [[ -d "$2" ]] && export "$1"="$2" }
+nvr() { [[ -d "$1" ]] && export PATH="$PATH:$1" }
 
 src $HOME/.bun/_bun
 
@@ -17,30 +14,30 @@ xpt GIT_CONFIG_GLOBAL "$HOME/.config/.gitconfig"
 # src "$ZDOTDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" # now in plugins
 
 # Path and sourcing
-if [ -d "$HOME/.local/share/adb-fastboot/platform-tools" ] ; then
-    xpt PATH "$PATH:$HOME/.local/share/adb-fastboot/platform-tools"
+if [ -d "$HOME/.local/share/adb-fastboot/platform-tools" ]; then
+    nvr "$HOME/.local/share/adb-fastboot/platform-tools"
 fi
 
 src "$HOME/.bun/_bun"
 xpt BUN_INSTALL "$HOME/.bun"
-xpt PATH "$PATH:$BUN_INSTALL/bin"
+nvr "$BUN_INSTALL/bin"
 
 xpt ANDROID_HOME "$HOME/Android/sdk"
-xpt PATH "$PATH:$ANDROID_HOME/emulator"
-xpt PATH "$PATH:$ANDROID_HOME/platform-tools"
+nvr "$ANDROID_HOME/emulator"
+nvr "$ANDROID_HOME/platform-tools"
 
 xpt FLYCTL_INSTALL "$HOME/.fly"
-xpt PATH "$PATH:$FLYCTL_INSTALL/bin"
+nvr "$FLYCTL_INSTALL/bin"
 
-xpt PATH "$PATH:$HOME/.turso"
+nvr "$HOME/.turso"
 
 xpt PNPM_HOME "$HOME/.local/share/pnpm"
 case ":$PATH:" in
     # find if this case is really necessary, if it isn't, remove the echo in the next line, else modify every $PATH update in this file to use a case statement
   *":$PNPM_HOME:"*) ;;
-  *) xpt PATH "$PATH:$PNPM_HOME" ;;
+  *) nvr "$PNPM_HOME" ;;
 esac
-xpt PATH "$PATH:$ZDOTDIR/custom/bin"
+nvr "$ZDOTDIR/custom/bin"
 
 # WSL browser, requires wslu installation to be followerd
 if [[ -n $WSL_DISTRO_NAME ]]; then
