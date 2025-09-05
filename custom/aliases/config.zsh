65b52b9ad54409705e896798f111f902c256ccef
luan-brav0 <luan-brav0@github.com>
2025-09-05 01:47:40 -0300

    fix(aliases/config): Capitalize env vars as && Remove excessive aliases for

--- custom/aliases/config.zsh
+++ custom/aliases/config.zsh
@@ -1,30 +1,17 @@
 # Easy access to frequently modified config files
 editproj() {
     if [[ $# -ne 1 ]] || [[ ! -d $1 ]]; then
-        ec ${yellow} "usage: editproj ${green}</path/to/project>{color}" && return 1
+        echocol ${yellow} "usage: editproj ${green}</path/to/project>{color}" && return 1
     fi
     nvim -c "cd $1" $1
 }
-alias zshconfig="editproj $zdotdir"
-alias zconfig="zshconfig"
-alias zconf="zconfig"
 
-alias nvimconfig="editproj $dotdir/nvim"
-alias nvimconf="nvimconfig"
-alias nvconf="nvimconfig"
-alias nvconfig="nvimconfig"
-alias vconf="nvimconfig"
-alias vconfig="nvimconfig"
-alias hyprconfig="editproj $dotdir/hypr"
-alias hconfig="editproj $dotdir/hypr"
-alias hconf="editproj $dotdir/hypr"
+alias zconf="editproj $ZDOTDIR"
 
-alias barconfig="editproj $dotdir/waybar"
-alias barconf="editproj $dotdir/waybar"
-alias wbconf="editproj $dotdir/waybar"
-alias bconf="editproj $dotdir/waybar"
+alias vconf="editproj $DOTDIR/nvim"
 
-alias wezconfig="editproj $dotdir/wezterm"
-alias termconfig="wezconfig"
-alias wezconf="termconfig"
-alias tconf="termconfig"
+alias hconf="editproj $DOTDIR/hypr"
+
+alias bconf="editproj $DOTDIR/waybar"
+
+alias tconf="editproj $DOTDIR/wezterm"
